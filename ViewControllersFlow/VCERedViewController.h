816a7225b9939ceaaa9cfd9ef60779e82ca64acd
mbutan <jagro-butan@Marcins-MacBook-Pro.local>
2016-01-26 12:58:57 +0100

    e with images

---
+++ ViewControllersFlow/VCERedViewController.h
@@ -0,0 +1,13 @@
+//
+//  VCERedViewController.h
+//  ViewControllersFlow
+//
+//  Created by Developer on 1/26/16.
+//  Copyright © 2016 Marcin Małysz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface VCERedViewController : UIViewController
+
+@end
---
+++ ViewControllersFlow/VCERedViewController.m
@@ -0,0 +1,40 @@
+//
+//  VCERedViewController.m
+//  ViewControllersFlow
+//
+//  Created by Developer on 1/26/16.
+//  Copyright © 2016 Marcin Małysz. All rights reserved.
+//
+
+#import "VCERedViewController.h"
+
+@interface VCERedViewController ()
+
+@end
+
+@implementation VCERedViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    
+    
+    
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
